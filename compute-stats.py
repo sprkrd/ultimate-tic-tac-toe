593ac24038567bde50bbaebb9a9fe5d01729c6d2
Alejandro Suarez <asuarez@iri.upc.edu>
2018-12-05 17:54:52 +0100

    improved script for measuring the performance between agents

--- compute-stats.py
+++ compute-stats.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python3
 
-from subprocess import check_output
-from sys import argv
 from tqdm import tqdm
 
-program1 = argv[1]
-program2 = argv[2]
+from subprocess import check_output
+from sys import argv
 
-N = 20
-wins_x = 0
-loses_x = 0
+from os.path import splitext, basename
 
-cmd = ("./server-9x9.x", program1, program2)
+def stats_str(N, program1, program2, wins):
+    program1 = splitext(basename(program1))[0]
+    program2 = splitext(basename(program2))[0]
+    return "Wins {} / Ties / Wins {}: {:.02%} / {:.02%} / {:.02%}".format(
+            program1, program2, wins[0]/N, (N-sum(wins))/N, wins[1]/N)
 
-for _ in tqdm(range(N)):
-    out = check_output(cmd).decode().strip()
-    if out == "Player 0 (x) WINS!":
-        wins_x += 1
-    elif out == "Player 1 (o) WINS!":
-        loses_x += 1
 
-print("Wins (%) / Ties (%) / Loses (%): {:.02f}% / {:.02f}% / {:.02f}%".format(
-        wins_x/N*100,(N-wins_x-loses_x)/N*100,loses_x/N*100))
+def main():
+    program1 = argv[1]
+    program2 = argv[2]
+
+    N = int(argv[3])
+    wins = [0, 0]
+
+    cmd1 = ("./server-9x9.x", program1, program2)
+    cmd2 = ("./server-9x9.x", program2, program1)
+
+    cmds = (cmd1, cmd2)
+
+    for idx in tqdm(range(N)):
+        cmd_idx = idx%2
+        out = check_output(cmds[cmd_idx]).decode().strip()
+        if out == "Player 0 (x) WINS!":
+            wins[cmd_idx] += 1
+        elif out == "Player 1 (o) WINS!":
+            wins[1-cmd_idx] += 1
+        tqdm.write(stats_str(idx+1,program1,program2,wins))
+
+    # print(stats_str(N, program1, program2, wins))
+
+
+if __name__ == "__main__":
+    main()
 
